365756d190f82eb992c9af783db53ccffbf14ca4
ThuHa307 <hatran3072003@gmail.com>
2025-04-19 16:36:18 +0700

    create project structure

---
+++ src/main/java/com/exe102/SwiftDrive/SwiftDriveApplication.java
@@ -0,0 +1,13 @@
+package com.exe102.SwiftDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe102/SwiftDrive/SwiftDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe102.SwiftDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
